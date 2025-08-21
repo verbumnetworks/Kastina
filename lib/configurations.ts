0292cdee9a5e00363da3a0e4da6186035ce985bf
Obiefuna Marcel Uchenna <obiefunamarcel@gmail.com>
2025-08-21 18:26:13 +0100

    Auth

--- lib/configurations.ts
+++ lib/configurations.ts
@@ -7,17 +7,7 @@ type RouteAccessMap = {
 export const routeAccessMap: RouteAccessMap = {
   "/admin(.*)": ["ADMIN"],
   "/student(.*)": ["BISHOP"],
-  "/teacher(.*)": ["teacher"],
-  "/parent(.*)": ["parent"],
+
   "/list/teachers": ["admin"],
-  "/list/students": ["admin", "teacher"],
-  "/list/parents": ["admin", "teacher"],
-  "/list/subjects": ["admin"],
-  "/list/classes": ["admin", "teacher"],
-  "/list/exams": ["admin", "teacher", "student", "parent"],
-  "/list/assignments": ["admin", "teacher", "student", "parent"],
-  "/list/results": ["admin", "teacher", "student", "parent"],
-  "/list/attendance": ["admin", "teacher", "student", "parent"],
-  "/list/events": ["admin", "teacher", "student", "parent"],
-  "/list/announcements": ["admin", "teacher", "student", "parent"],
+
 };
--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,32 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+// import { getToken } from "next-auth/jwt";
+// import { NextResponse } from "next/server";
 
-import { NextRequest } from "next/server";
-import { routeAccessMap } from "./lib/configurations";
-const secret = process.env.NEXTAUTH_SECRET;
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret });
-  const currentPath = req.nextUrl.pathname;
-  if (!token) return NextResponse.next();
-  const role = token.role as string;
-  if (token && currentPath === "/") {
-    return NextResponse.redirect(new URL(`/${role}`, req.url));
-  }
-  for (const route in routeAccessMap) {
-    const matcher = new RegExp(`^${route}$`);
-    if (matcher.test(currentPath)) {
-      const allowedRoles = routeAccessMap[route];
-      if (!allowedRoles.includes(role)) {
-        const redirectUrl = new URL(`/${role}`, req.url);
-        return NextResponse.redirect(redirectUrl);
-      }
-    }
-  }
-  return NextResponse.next();
-}
-export const config = {
-  matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
-  ],
-};
+// import { NextRequest } from "next/server";
+// import { routeAccessMap } from "./lib/configurations";
+// const secret = process.env.NEXTAUTH_SECRET;
+// export async function middleware(req: NextRequest) {
+//   const token = await getToken({ req, secret });
+//   const currentPath = req.nextUrl.pathname;
+//   if (!token) return NextResponse.next();
+//   const role = token.role as string;
+//   if (token && currentPath === "/") {
+//     return NextResponse.redirect(new URL(`/${role}`, req.url));
+//   }
+//   for (const route in routeAccessMap) {
+//     const matcher = new RegExp(`^${route}$`);
+//     if (matcher.test(currentPath)) {
+//       const allowedRoles = routeAccessMap[route];
+//       if (!allowedRoles.includes(role)) {
+//         const redirectUrl = new URL(`/${role}`, req.url);
+//         return NextResponse.redirect(redirectUrl);
+//       }
+//     }
+//   }
+//   return NextResponse.next();
+// }
+// export const config = {
+//   matcher: [
+//     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+//     "/(api|trpc)(.*)",
+//   ],
+// };
